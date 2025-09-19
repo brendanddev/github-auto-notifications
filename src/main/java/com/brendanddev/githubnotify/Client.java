2a4385a81247bf5780da488440c549b8ba86703e
Brendan Dileo <dileob23@gmail.com>
2025-09-19 09:47:19 -0400

    Update Client to send basic GET request

--- src/main/java/com/brendanddev/githubnotify/Client.java
+++ src/main/java/com/brendanddev/githubnotify/Client.java
@@ -1,7 +1,44 @@
 package com.brendanddev.githubnotify;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class Client {
+
+    private String uri = "https://api.github.com";
+    private ObjectMapper mapper = new ObjectMapper();
+
+    String key = System.getenv("GITHUB_TOKEN");
+
+
+
+
+
+
+    public void sendGET(String uri) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(uri))
+            .header("Accept", "application/json")
+            .build();
+        
+        HttpResponse<String> response = 
+            client.send(request, BodyHandlers.ofString());
+
+        JsonNode jsonNode = mapper.readTree(response.body());
+        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+        String prettyJson = writer.writeValueAsString(jsonNode);
+        System.out.println(prettyJson);
+    }
+
+
     
 }
