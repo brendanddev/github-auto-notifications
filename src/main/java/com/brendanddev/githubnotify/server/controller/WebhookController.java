2bccca7552b134e7e92ea52734c65542345bf276
Brendan Dileo <dileob23@gmail.com>
2025-09-22 16:28:55 -0400

    Setup webhook controller

--- src/main/java/com/brendanddev/githubnotify/server/controller/WebhookController.java
+++ src/main/java/com/brendanddev/githubnotify/server/controller/WebhookController.java
@@ -1,5 +1,19 @@
 package com.brendanddev.githubnotify.server.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webhook")
 public class WebhookController {
+
+    @PostMapping
+    public ResponseEntity<String> receiveWebhook(@RequestBody String payload) {
+        System.out.println(payload);
+        return ResponseEntity.ok("Received");
+    }
     
 }
